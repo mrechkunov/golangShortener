3f6e6e640fe1c858a4dc61f32450bdec05b3507e
Michael Rechkunov <mrechkunov@gmail.com>
2026-02-17 11:27:08 +0300

    storage utest add

---
+++ internal/repository/storage_test.go
@@ -0,0 +1,49 @@
+package repository_test
+
+import (
+	"testing"
+
+	"github.com/mrechkunov/golangShortener.git/internal/repository"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInsertData(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		url      string
+		shortURL string
+	}{
+		{"Valid name", "http://test.test", "http://test.short"},
+		{"Empty name", "", ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repository.Storage.SetData(tt.url, tt.shortURL)
+		})
+	}
+}
+
+func TestSelectData(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		shortURL string
+		want     string
+	}{
+		{"Valid name", "http://test.test", "http://test.test"},
+		{"Empty name", "", ""},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repository.Storage.SetData(tt.want, tt.shortURL)
+			got, _ := repository.Storage.GetData(tt.shortURL)
+
+			// TODO: update the condition below to compare got with tt.want.
+			if !assert.Equal(t, tt.want, got) {
+				t.Errorf("SelectData() = %v want %v", got, tt.want)
+			}
+		})
+	}
+}
