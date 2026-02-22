8f524b86c02e4ee848429fd1f07b1ba4b2e6ac3a
Michael Rechkunov <mrechkunov@gmail.com>
2026-02-22 17:43:06 +0300

    iter 8 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/mrechkunov/golangShortener.git/internal/config"
@@ -38,48 +37,5 @@ func main() {
 	if err := http.ListenAndServe(config.ConfigAdreses.ServerBindAdress, r); err != nil {
 		// записываем в лог ошибку, если сервер не запустился
 		logger.Sugar.Fatalw(err.Error(), "event", "start server")
-	}
-}
-
-func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// по умолчанию устанавливаем оригинальный http.ResponseWriter как тот,
-		// который будем передавать следующей функции
-		ow := w
-
-		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
-		acceptEncoding := r.Header.Get("Accept-Encoding")
-		supportsGzip := strings.Contains(acceptEncoding, "gzip")
-		// проверяем что данные application/json или text/html
-		dataFormat := r.Header.Get("Content-type")
-		jsonDataFormat := strings.Contains(dataFormat, "application/json")
-		textHTMLDataFormat := strings.Contains(dataFormat, "text/html")
-
-		if supportsGzip && (jsonDataFormat || textHTMLDataFormat) {
-			// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
-			cw := newCompressWriter(w)
-			// меняем оригинальный http.ResponseWriter на новый
-			ow = cw
-			// не забываем отправить клиенту все сжатые данные после завершения middleware
-			defer cw.Close()
-		}
-
-		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
-		contentEncoding := r.Header.Get("Content-Encoding")
-		sendsGzip := strings.Contains(contentEncoding, "gzip")
-		if sendsGzip {
-			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
-			cr, err := newCompressReader(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			// меняем тело запроса на новое
-			r.Body = cr
-			defer cr.Close()
-		}
-
-		// передаём управление хендлеру
-		h.ServeHTTP(ow, r)
 	}
 }
