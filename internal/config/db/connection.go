306c6f26bcc15c1ab981bd5bd40170efcc086443
Michael Rechkunov <mrechkunov@gmail.com>
2026-03-02 14:11:14 +0300

    iter10 to autotest2

--- internal/config/db/connection.go
+++ internal/config/db/connection.go
@@ -3,13 +3,14 @@ package db
 import (
 	"database/sql"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/mrechkunov/golangShortener.git/internal/config"
 	"github.com/mrechkunov/golangShortener.git/internal/logger"
 )
 
 func Connect() (*sql.DB, error) {
 	connStr := config.ConfigAdreses.DBConnStr
-	db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("pgx", connStr)
 	if err != nil {
 		logger.Log.Errorln(err)
 	}
