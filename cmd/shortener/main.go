2df01009db08c4aed08e90fa94e0de342745dda2
Michael Rechkunov <mrechkunov@gmail.com>
2026-05-05 13:47:09 +0300

    iter 15 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -36,4 +36,5 @@ func main() {
 	if err := http.ListenAndServe(config.ConfigAdreses.ServerBindAdress, r); err != nil {
 		logger.Log.Fatalw(err.Error(), "event", "start server")
 	}
+	close(chanToDelete)
 }
