15812d788ea8672e3b559266a9afb0c58da458cc
Michael Rechkunov <mrechkunov@gmail.com>
2026-06-19 16:07:46 +0300

    iter 25 test

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,21 +1,23 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/mrechkunov/golangShortener.git/internal/logger"
 )
 
 type Adreses struct {
-	ServerBindAdress   string
-	ResultServerAdress string
+	ServerBindAdress   string `json:"server_address"`
+	ResultServerAdress string `json:"base_url"`
 	MigrationsPath     string
-	JSONFile           string
+	JSONFile           string `json:"file_storage_path"`
 	DBConnStr          string
 	AuditFile          string
 	AuditUrl           string
-	HttpsEnable        bool
+	HttpsEnable        bool `json:"enable_https"`
 }
 
 var Fmem *os.File
@@ -27,9 +29,13 @@ var ConfigAdreses = Adreses{
 	AuditFile:          "",
 	AuditUrl:           "",
 }
+
 var PublisherAudit logger.Audit
+var ConfigFileAddress string
+var ConfigFileData Adreses
 
 func Init() {
+	cf := flag.String("c", "", "config file address")
 	ba := flag.String("a", "localhost:8080", "adress to server run")
 	ra := flag.String("b", "http://localhost:8080", "default responce server adress")
 	mp := flag.String("m", "file://migrations", "default migration PATH")
@@ -43,20 +49,49 @@ func Init() {
 	flag.Parse()
 
 	// если переиенные окружения установленны, берем их, иначе берем флаг
+
+	if cfAddress, isConfigFileAddress := os.LookupEnv("CONFIG"); isConfigFileAddress {
+		ConfigFileAddress = cfAddress
+	} else {
+		ConfigFileAddress = *cf
+	}
+	fmt.Println("config file address:", ConfigFileAddress)
+	// если есть адрес конфига, парсим сначала его и присваеваем все значения структуре конфигурации
+	if ConfigFileAddress != "" {
+		// Считайтываем файл целиком
+		data, err := os.ReadFile(ConfigFileAddress)
+		if err != nil {
+			logger.Log.Warnln("Ошибка чтения файла:", err)
+		}
+		// Распарсим JSON в структуру
+		err = json.Unmarshal(data, &ConfigFileData)
+		if err != nil {
+			logger.Log.Warnln("Ошибка парсинга JSON:", err)
+		}
+	}
 	if serverAddress, isEnvBindSrv := os.LookupEnv("SERVER_ADDRESS"); isEnvBindSrv {
 		ConfigAdreses.ServerBindAdress = serverAddress
 	} else {
 		ConfigAdreses.ServerBindAdress = *ba
+	}
+	if ConfigAdreses.ServerBindAdress == "localhost:8080" && ConfigFileData.ServerBindAdress != "" {
+		ConfigAdreses.ServerBindAdress = ConfigFileData.ServerBindAdress
 	}
 	if _, isEnvHttpsEnable := os.LookupEnv("ENABLE_HTTPS"); isEnvHttpsEnable {
 		ConfigAdreses.HttpsEnable = true
 	} else {
 		ConfigAdreses.HttpsEnable = *se
 	}
+	if !ConfigAdreses.HttpsEnable && ConfigFileData.HttpsEnable {
+		ConfigAdreses.HttpsEnable = true
+	}
 	if baseURL, isEnvResSrv := os.LookupEnv("BASE_URL"); isEnvResSrv {
 		ConfigAdreses.ResultServerAdress = baseURL
 	} else {
 		ConfigAdreses.ResultServerAdress = *ra
+	}
+	if ConfigAdreses.ResultServerAdress == "http://localhost:8080" && ConfigFileData.ResultServerAdress != "" {
+		ConfigAdreses.ResultServerAdress = ConfigFileData.ResultServerAdress
 	}
 
 	if migratoinsPath, isEnvMigrationsPath := os.LookupEnv("MIGRATIONS_PATH"); isEnvMigrationsPath {
@@ -70,6 +105,10 @@ func Init() {
 	} else {
 		ConfigAdreses.JSONFile = *jf
 	}
+	if ConfigAdreses.JSONFile == "" && ConfigFileData.JSONFile != "" {
+		ConfigAdreses.JSONFile = ConfigFileData.JSONFile
+	}
+
 	if dbConnStr, isEnvDBConnStr := os.LookupEnv("DATABASE_DSN"); isEnvDBConnStr {
 		ConfigAdreses.DBConnStr = dbConnStr
 	} else {
