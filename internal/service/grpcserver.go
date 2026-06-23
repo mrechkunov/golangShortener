1c95b04a14926e16af8858465a4355a83960e9b7
Michael Rechkunov <mrechkunov@gmail.com>
2026-06-23 11:39:35 +0300

    iter 28 test3

--- internal/service/grpcserver.go
+++ internal/service/grpcserver.go
@@ -1,43 +1,74 @@
 package service
 
 import (
+	"context"
 	"errors"
 
+	"github.com/mrechkunov/golangShortener.git/internal/config"
+	"github.com/mrechkunov/golangShortener.git/internal/cryptoauth"
+	"github.com/mrechkunov/golangShortener.git/internal/logger"
+	"github.com/mrechkunov/golangShortener.git/internal/model"
 	"github.com/mrechkunov/golangShortener.git/internal/repository"
 	pb "github.com/mrechkunov/golangShortener.git/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
-type gRPCServer struct {
+type GRPCServer struct {
 	pb.UnimplementedShortenerServiceServer
 }
 
 // ListUserURLs return to user all urls where user is creator
-func (g gRPCServer) ListUserURLs() (rez pb.UserURLsResponse) {
+func (g GRPCServer) ListUserURLs(ctx context.Context) (out *pb.UserURLsResponse, err error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "no metadata found")
+	}
+	var uid uint32
+	if values := md["authorization"]; len(values) > 0 {
+		token := values[0]
+		uid, err = cryptoauth.GetIDFromCookie(token)
+		if err != nil {
+			logger.Log.Infoln("no ID in metadata")
+			return nil, status.Error(codes.InvalidArgument, "error while get ID in metadata")
+		}
+	}
+	// TODO:как задать в метаданные сгенерированный новый токен ?
 
+	// Выбрать из хранилища все записи с uid
+	responseBatch := repository.GetStorage().GetDataByUID(uid)
+	// добавляем всем префикс базового адреса
+	baseResultAdress := config.ConfigAdreses.ResultServerAdress
+	var result []model.ResponseDataBatchByCookie
+	for _, rb := range responseBatch {
+		rb.ShortURL = baseResultAdress + "/" + rb.ShortURL
+
+		result = append(result, rb)
+	}
 	return
 }
 
 // ExplandURLS return original url if short url is exist in DB and not set as deleted
-func (g gRPCServer) ExpandURL(in pb.URLExpandRequest) (rez pb.URLExpandResponse, err error) {
+func (g GRPCServer) ExpandURL(ctx context.Context, in *pb.URLExpandRequest) (out *pb.URLExpandResponse, err error) {
 	shortURL := in.GetId()
 	shortURL = shortURL[1:]
 	longURL, isFound := repository.GetStorage().GetData(shortURL)
 	if repository.GetStorage().IsDeleted(shortURL) {
 		err = errors.New("short URL is deleted")
-		rez.SetResult("")
+		out.SetResult("")
 		return
 	}
 	if !isFound {
 		err = errors.New("short URL not found")
 		return
 	}
-	rez.SetResult(longURL)
+	out.SetResult(longURL)
 	return
 }
 
 // ShortenURL shorting url from json and insert it in DB
-func (g gRPCServer) ShortenURL(in pb.URLShortenRequest) (rez pb.URLShortenResponse) {
-	in.GetUrl()
+func (g GRPCServer) ShortenURL(ctx context.Context, in *pb.URLShortenRequest) (out *pb.URLShortenResponse) {
 
 	return
 }
